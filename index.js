5ad181adbc3cf1a65bc4d2947471601d91822cf8
maycolroa <lehallo@hotmail.com>
2022-07-28 23:47:53 -0500

    node.js  28/07/22 0.00

---
+++ index.js
@@ -0,0 +1,37 @@
+const express=require('express');
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) =>{
+    res.send("Hola mi server en Express");
+});
+
+app.get("/nueva_ruta", (req, res) =>{
+    res.send("Hola, soy una nueva ruta");
+});
+
+app.get("/products", (req, res) =>{
+    res.json([
+        {
+        name : "producto_1",
+        price : 1000
+    },
+    {
+        name : "producto_2",
+        price : 2000
+    }
+    ]);
+});
+
+app.get("/products/:id", (req, res) =>{
+const { id } = req.params;
+res.json({
+    id,
+    name : "producto_2",
+    price : 2000
+});
+})
+
+app.listen(port, () =>{
+    console.log("My port: " + port);
+});
